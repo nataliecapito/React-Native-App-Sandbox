2922714e38707e6e1d5f5da91edaf64cb9df2f27
smacknat <nattyjo73@gmail.com>
2018-11-07 19:41:33 -0500

    style sheet created for home

---
+++ content/javascript/mobile/main/styles/mainstyles.js
@@ -0,0 +1,11 @@
+// general: styles for home.js.
+
+// node packages
+import { StyleSheet, Platform, Dimensions } from 'react-native'
+
+// Dimensions global style values
+const widthDimensions = Dimensions.get('window').width
+
+export default StyleSheet.create({
+  // ---------- home.js styles
+})
