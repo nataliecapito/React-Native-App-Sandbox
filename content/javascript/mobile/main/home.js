0cd822520662f8ff28f9c443f9fa978a9caf1682
smacknat <nattyjo73@gmail.com>
2018-11-07 19:45:19 -0500

    color

--- content/javascript/mobile/main/home.js
+++ content/javascript/mobile/main/home.js
@@ -13,7 +13,7 @@ export default class MainHome extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Hello, World!</Text>
+        <Text style={{ color: 'blue' }}>Hello, World!</Text>
       </View>
     )
   }
--- content/javascript/mobile/main/styles/mainstyles.js
+++ content/javascript/mobile/main/styles/mainstyles.js
@@ -1,10 +1,7 @@
 // general: styles for home.js.
 
 // node packages
-import { StyleSheet, Platform, Dimensions } from 'react-native'
-
-// Dimensions global style values
-const widthDimensions = Dimensions.get('window').width
+import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
   // ---------- home.js styles
